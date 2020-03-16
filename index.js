8aad00a5df6a7ca22d6c104a3ad2d868cb6b0ea0
Thomas Hodges <me@thodges.com>
2020-03-16 13:21:08 -0500

    Handle errors from API and in index calculation

--- index.js
+++ index.js
@@ -1,74 +1,69 @@
-const rp = require('request-promise');
-const snx = require('synthetix');
-const Decimal = require('decimal.js');
+const rp = require('request-promise')
+const snx = require('synthetix')
+const Decimal = require('decimal.js')
 
 const getPriceData = async (synth) => {
-  return rp({
-    url: `https://web3api.io/api/v2/market/prices/${synth.toLowerCase()}/latest`,
-    headers: {
-      'X-API-KEY': process.env.API_KEY
-    },
-    json: true
-  })
-};
+  try {
+    return await rp({
+      url: `https://web3api.io/api/v2/market/prices/${synth.toLowerCase()}/latest`,
+      headers: {
+        'X-API-KEY': process.env.API_KEY
+      },
+      json: true
+    })
+  } catch (error) {
+    throw error.message
+  }
+}
 
 const calculateIndex = (indexes) => {
-  let value = new Decimal(0);
-  indexes.forEach(i => {
-    const price = i.priceData.payload[`${i.symbol.toLowerCase()}_usd`].price;
-    if (price <= 0)
-      throw "invalid price";
-    value = value.plus(new Decimal(i.units).times(new Decimal(price)))
-  });
+  let value = new Decimal(0)
+  try {
+    indexes.forEach(i => {
+      value = value.plus(new Decimal(i.units).times(new Decimal(i.priceData.payload[`${i.symbol.toLowerCase()}_usd`].price)))
+    })
+  } catch (error) {
+    throw error.message
+  }
   return value.toNumber()
-};
+}
 
 const createRequest = async (input, callback) => {
-  const asset = input.data.asset || 'sCEX';
-  const datas = snx.getSynths({network: 'mainnet'}).filter(({index, inverted}) => index && !inverted);
-  const data = datas.find(d => d.name.toLowerCase() === asset.toLowerCase());
-  await Promise.all(data.index.map(async (synth) => {
-    synth.priceData = await getPriceData(synth.symbol)
-  })).catch(err => {
+  const asset = input.data.asset || 'sCEX'
+  const datas = snx.getSynths({network: 'mainnet'}).filter(({index, inverted}) => index && !inverted)
+  const data = datas.find(d => d.name.toLowerCase() === asset.toLowerCase())
+  try {
+    await Promise.all(data.index.map(async (synth) => {
+      synth.priceData = await getPriceData(synth.symbol)
+    }))
+    data.result = calculateIndex(data.index)
+    callback(200, {
+      jobRunID: input.id,
+      data: data,
+      result: data.result,
+      statusCode: 200
+    })
+  } catch (error) {
     callback(500, {
       jobRunID: input.id,
       status: 'errored',
-      error: err,
+      error: error,
       statusCode: 500
     })
-  });
-
-  try {
-    data.result = calculateIndex(data.index);
-  } catch (e) {
-    callback(500, {
-      jobRunID: input.id,
-      status: 'errored',
-      error: "failed getting price",
-      statusCode: 500
-    });
-    return
   }
-
-  callback(200, {
-    jobRunID: input.id,
-    data: data,
-    result: data.result,
-    statusCode: 200
-  })
-};
+}
 
 exports.gcpservice = (req, res) => {
   createRequest(req.body, (statusCode, data) => {
     res.status(statusCode).send(data)
   })
-};
+}
 
 exports.handler = (event, context, callback) => {
   createRequest(event, (statusCode, data) => {
     callback(null, data)
   })
-};
+}
 
 exports.handlerv2 = (event, context, callback) => {
   createRequest(JSON.parse(event.body), (statusCode, data) => {
@@ -78,6 +73,6 @@ exports.handlerv2 = (event, context, callback) => {
       isBase64Encoded: false
     })
   })
-};
+}
 
-module.exports.createRequest = createRequest;
+module.exports.createRequest = createRequest
